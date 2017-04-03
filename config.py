af49683c3aad7f558fc435a11776bfb64f3a24fc
muzixing <350959853@qq.com>
2017-04-03 11:36:48 -0700

    use double quotes in config

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
-UCS = '<UCS Manager IP address in single quotes>'
-UCS_USER = '<UCS Manager user name in single quotes>'
-UCS_PASSWD = '<UCS Manager password in single quotes>'
+UCS = "<UCS Manager IP address in double quotes>"
+UCS_USER = "<UCS Manager user name in double quotes>"
+UCS_PASSWD = "<UCS Manager password in double quotes>"
 
 # For HTTPS connection, please set IS_SECURE=True, and set PORT, default port number is 443
 # For HTTP connection, pleases et IS_SECURE=False and set PORT, default port number is 80.
 IS_SECURE = True
 PORT = 443
 
-LOG_LEVEL = 'INFO'
+LOG_LEVEL = "INFO"
 
-ZEUS_TOKEN = '<INSERT_TOKEN_HERE>'
+ZEUS_TOKEN = "<INSERT_TOKEN_HERE>"
 ZEUS_SERVER = "<Zeus API URL in double quotes>"
